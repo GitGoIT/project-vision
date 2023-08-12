19e78086cf13fbb85e61738d34a9cbdf05aaa6d5
Ivan Alimenko <ivan.alimenko@ukr.net>
2023-08-12 16:22:58 +0300

    add_components

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+// import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+// import About from "./components/About/About";
+// import Problem from "./components/Problem/Problem";
+// import Solution from "./components/Solution/Solution";
+// import Samples from "./components/Samples/Samples";
+// import Steps from "./components/Steps/Steps";
+// import Team from "./components/Team/Team";
+// import Contacts from "./components/Contacts/Contacts";
+// import Feedback from "./components/Feedback/Feedback";
+
+// import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <>
+      {/* <Header /> */}
+      <main>
+        <Hero />
+        {/* <About />
+        <Problem />
+        <Solution />
+        <Samples />
+        <Steps />
+        <Team />
+        <Contacts />
+        <Feedback /> */}
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import css from "./Hero.module.css";
+
+
+const Hero = () => {
+
+  return (
+    <section className={css.section}>
+      <div className={css.titleWrapper}>
+        <div className={css.dronWrapper}></div>
+        <h1 className={css.title}>
+          MILITARY OBJECTS <span className={css.titleSpan}>DETECTION</span>
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
